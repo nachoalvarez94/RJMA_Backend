27502ab97d547167ab2bc8488f3cc194cfc0f666
NACHO_PC\nacho <nachoalvarez94@gmail.com>
2026-04-21 12:44:39 +0200

    no message

---
+++ src/main/java/com/rjma/RjmaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rjma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RjmaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RjmaBackendApplication.class, args);
+	}
+
+}
